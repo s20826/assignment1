b2a3f2a5fb68176144163ba17c0c87fa3a4e0457
s20826 <s20826@pjwstk.edu.pl>
2023-04-24 15:47:58 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "./components/header/header";
 import Episodes from "./pages/episodes/episodes";
+import Footer from './components/footer/footer';
 
 const App: React.FC = () => {
   return (
       <div>
           <Header/>
           <Episodes/>
+          <Footer/>
       </div>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './footer.css'
+
+export default function Header() {
+    return (
+       <footer className='d-grid'>
+           <div className='footer-text'>
+               LOREM IPSUM ©2021
+           </div>
+       </footer>
+    )
+}
