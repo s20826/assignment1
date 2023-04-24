1235ffd3247a7fd9eaddf0212a32edfd570df662
s20826 <s20826@pjwstk.edu.pl>
2023-04-24 14:00:13 +0200

    Add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './common/fonts/Lato-Black.ttf'
+import './common/fonts/Lato-Bold.ttf'
+import './common/fonts/Lato-Regular.ttf'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
