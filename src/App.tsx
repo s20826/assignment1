2c05b6e1a2f8512386658aec5aee82a23b03d801
s20826 <s20826@pjwstk.edu.pl>
2023-04-24 13:58:04 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from "./components/header/header";
 
 const App: React.FC = () => {
   return (
       <div>
+          <Header/>
       </div>
   );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './header.css'
+
+
+const Header: React.FC = () => {
+    return (
+        <header className='d-grid align-content-center justify-content-center
+        justify-content-md-start' >
+            <p className='text-nowrap'>
+                LOREM IPSUM
+            </p>
+        </header>
+    )
+}
+export default Header;
